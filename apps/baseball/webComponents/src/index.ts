835b438ba2ee8086d74daec4e42ad6cda470dbc1
tonyarcher <46615418+tonyarcher@users.noreply.github.com>
2026-07-30 21:51:19 -0500

    feat(webComponents): add <baseball-scorebook-grid> custom element to webComponents package

--- apps/baseball/webComponents/src/index.ts
+++ apps/baseball/webComponents/src/index.ts
@@ -1,4 +1,5 @@
 import './scoreboard/baseball-scoreboard.js';
 import './scorebook/baseball-defense-diagram.js';
+import './scorebook/baseball-scorebook-grid.js';
 import './scoring/baseball-matchup-card.js';
 import './scoring/baseball-action-grid.js';
